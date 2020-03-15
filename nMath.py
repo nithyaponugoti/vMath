0dba98c43a09d20ece24f6ee96efb6c5da8fda76
nponugoti <nag.ponugoti@fungible.com>
2020-03-15 16:22:43 -0700

    cleanup timer support code

--- nMath.py
+++ nMath.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python2.7
 
+import sys
 import random
+import time
 import datetime
 import argparse
 import threading
+import signal
+import os
 
 ADD = 0 
 SUB = 1
@@ -33,21 +37,83 @@ class test_report():
         minutes = int(time_taken / 60)
         seconds = int(time_taken % 60)
 
-        print("\n\n=================================")
+        print("\n\n==============================================")
+        print('Test Finished! time: {}'.format(
+            datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
         print("Minutes: {0} Seconds: {1}".format(minutes, seconds))
-        print("=================================")
+        print("=================================================")
         print("\nScore:\nCorrect: {0} Wrong: {1}".format(
             self.get_score(), self.num_q-self.get_score()))
-        print("=================================")
+        print("=================================================")
+
+    def test_end(self):
+        self.print_report
+        sys.exit(0)
+
+class thread_timer(threading.Thread):
+    def __init__(self, timeout_duration):
+        threading.Thread.__init__(self)
+        self.timeout_duration = timeout_duration
+        self.kill_received = False
+
+    def run(self):
+        print 'Starting timer for {} sec'.format(self.timeout_duration)
+        self.active = True
+        while ((not self.kill_received) and (self.timeout_duration)):
+            time.sleep(1)
+            self.timeout_duration -=1
+        self.active = False
+
+        if self.timeout_duration == 0:
+            try:
+                raise TimerTimedOut
+            except TimerTimedOut as timeout:
+                print '\n**** TIMEOUT ****'
+
+
+    def alive(self):
+        return self.active
+
+    def cancel(self):
+        print 'Stopping the timer'
+        self.kill_received = True
+
+
+class TestExit(Exception):
+    """
+    Custom exception which is used to trigger the clean exit
+    of all running threads and the main program.
+    """
+    pass
+
+
+def test_stop(signum, frame):
+    #print('Caught signal %d' % signum)
+    raise TestExit
+
+class TimerTimedOut(Exception):
+    pass
 
 def test(q_type, num_q, q_level, timeout_period):
+
+    signal.signal(signal.SIGTERM, test_stop)
+    signal.signal(signal.SIGINT, test_stop)
+
     oper = q_type
+
+    # Convert minutes to seconds
+    timeout_period = timeout_period * 60
     report = test_report(timeout_period, num_q)
 
     if timeout_period:
-        timer = threading.Timer(timeout_period,
-                report.print_report) 
-        timer.start()
+        timer = thread_timer(timeout_period)
+        try:
+            timer.start()
+        except Exception, e:
+            print 'Exception: {}'.format(e)
+
+    print('\n\n*** Test started! time: {} ***\n'.format(
+        datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
 
 
     rand_oper_start = ADD 
@@ -117,50 +183,67 @@ def test(q_type, num_q, q_level, timeout_period):
         print 'Invalid question level: {}'.format(q_level)
         sys.exit(1)
 
-    for i in range(num_q):
-        if q_type == RANDOM:
-            oper = random.randint(rand_oper_start, rand_oper_end)
- 
-        if oper == ADD:
-            x = random.randint(add_operand1_min, add_operand1_max)
-            y = random.randint(add_operand2_min, add_operand2_max)
-            answer = x + y
-            oper_str = "+"
-        elif oper == SUB:
-            x = random.randint(sub_operand1_min, sub_operand1_max)
-            y = random.randint(sub_operand2_min, x)
-            answer = x - y
-            oper_str = "-"
-        elif oper == MUL:
-            x = random.randint(mul_operand1_min, mul_operand1_max)
-            y = random.randint(mul_operand2_min, mul_operand2_max)
-            answer = x * y
-            oper_str = "x"
-        elif oper == DIV:
-            y = random.randint(dev_operand1_min, dev_operand1_max)
-            answer = random.randint(dev_operand2_min, dev_operand2_max)
-            x = y * answer 
-            oper_str = "/"
-        else:
-            print("Invalid operation type!");
+    try:
+        for i in range(num_q):
+            if q_type == RANDOM:
+                oper = random.randint(rand_oper_start, rand_oper_end)
 
-        while True:
-            try:
-                n = int(raw_input("\nQuestion {0}\n{1} {2} {3} = ".format(
-                        i+1, x, oper_str, y)))
-                break
-            except ValueError as verr:
-                print("Inavalid number! Enter correct number!")
-            except Exception as ex:
-                print("Inavalid number! Enter correct number!")
+            if oper == ADD:
+                x = random.randint(add_operand1_min, add_operand1_max)
+                y = random.randint(add_operand2_min, add_operand2_max)
+                answer = x + y
+                oper_str = "+"
+            elif oper == SUB:
+                x = random.randint(sub_operand1_min, sub_operand1_max)
+                y = random.randint(sub_operand2_min, x)
+                answer = x - y
+                oper_str = "-"
+            elif oper == MUL:
+                x = random.randint(mul_operand1_min, mul_operand1_max)
+                y = random.randint(mul_operand2_min, mul_operand2_max)
+                answer = x * y
+                oper_str = "x"
+            elif oper == DIV:
+                y = random.randint(dev_operand1_min, dev_operand1_max)
+                answer = random.randint(dev_operand2_min, dev_operand2_max)
+                x = y * answer
+                oper_str = "/"
+            else:
+                print("Invalid operation type!");
 
-        if answer == n:
-            print("Great..Answer is correct!!!")
-            report.increse_score()
-        else:
-            print("Sorry...Answer is Wrong. Correct answer is {0}".format(answer))
+            while True:
+                try:
+                    n = int(raw_input(
+                        "\nQuestion {0}\n{1} {2} {3} = ".format(
+                            i+1, x, oper_str, y)))
+                    break
+                except TestExit as test_exit:
+                    report.print_report()
+                    timer.cancel()
+                    sys.exit(1)
+                except TimerTimedOut as timeout:
+                    print 'TIMEOUT!'
+                    report.print_report()
+                    timer.cancel()
+                    sys.exit(1)
+                except ValueError as verr:
+                    print("Inavalid number! Enter correct number!")
+                except Exception as ex:
+                    print("Inavalid number! Enter correct number!")
+
+            if answer == n:
+                print("Great..Answer is correct!!!")
+                report.increse_score()
+            else:
+                print("Sorry...Answer is Wrong. Correct answer is {0}".format(answer))
+    except KeyboardInterrupt:
+        # Ctrl-C handling and send kill to threads
+        print 'Test is killed!'
+        timer.cancel()
 
     report.print_report()
+    timer.cancel()
+    sys.exit(0)
 
 def time_duration(x):
     x = int(x)
@@ -177,12 +260,12 @@ def main():
     parser.add_argument("-d", help="Divisions", dest='div', action="store_true")
     parser.add_argument("-r", help="Random questions", dest='random', action="store_true")
     arg_parser.add_argument("-n", help="Number of questions", dest="num_q", required=True, type=int, choices=[5, 10, 20, 40, 50, 100, 200])
-    arg_parser.add_argument("-t", help="time duration", dest="time_duration", type=time_duration)
+    arg_parser.add_argument("-t", help="time duration in minutes", dest="time_duration", type=time_duration)
     arg_parser.add_argument("-l", help="Level of difficulty", dest="level", required=True, choices=['basic', 'medium', 'high'])
 
     args = arg_parser.parse_args()
 
-    print args
+    # print args
 
     if args.add:
         test(ADD, args.num_q, args.level, args.time_duration)
